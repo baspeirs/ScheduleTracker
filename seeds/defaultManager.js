bb443305aec02683ba188117c2ccdc8292d6f1f2
Bryce Speirs <baspeirs@gmail.com>
2020-10-26 10:14:05 -0400

    added seed files for deployment

---
+++ seeds/defaultManager.js
@@ -0,0 +1,8 @@
+module.exports = 
+    {
+        username: "user1",
+        email: "manjoe@company.com",
+        name: "Manager Joe",
+        manager: true,
+        password: "password1"
+    }
---
+++ seeds/schedule.js
@@ -0,0 +1,391 @@
+module.exports = 
+    {
+        week: 12,
+        days: [
+            {
+                day: "Monday",
+                employees: [
+                    {
+                        name: "Bryce",
+                        type: "Driver",
+                        shift: "12-8"
+                    },
+                    {
+                        name: "John",
+                        type: "Driver",
+                        shift: "10-1"
+                    },
+                    {
+                        name: "Aaron",
+                        type: "Driver",
+                        shift: "11-2"
+                    },
+                    {
+                        name: "",
+                        type: "Driver",
+                        shift: "11-3"
+                    },
+                    {
+                        name: "",
+                        type: "Driver",
+                        shift: "11-3"
+                    },
+                    {
+                        name: "Jed",
+                        type: "In-Shop",
+                        shift: "11-2"
+                    },
+                    {
+                        name: "",
+                        type: "In-Shop",
+                        shift: "11-3"
+                    },
+                    {
+                        name: "",
+                        type: "In-Shop",
+                        shift: "11-3"
+                    },
+                    {
+                        name: "Zach",
+                        type: "Manager",
+                        shift: "7-3"
+                    },
+                    {
+                        name: "Micheal",
+                        type: "Manager",
+                        shift: "12-8"
+                    }
+                ]
+            },
+            {
+                day: "Tuesday",
+                employees: [
+                    {
+                        name: "James",
+                        type: "Driver",
+                        shift: "11-3"
+                    },
+                    {
+                        name: "Justin",
+                        type: "Driver",
+                        shift: "10-2"
+                    },
+                    {
+                        name: "Aaron",
+                        type: "Driver",
+                        shift: "12-5"
+                    },
+                    {
+                        name: "",
+                        type: "Driver",
+                        shift: "11-3"
+                    },
+                    {
+                        name: "",
+                        type: "Driver",
+                        shift: "11-3"
+                    },
+                    {
+                        name: "Jed",
+                        type: "In-Shop",
+                        shift: "11:30-8"
+                    },
+                    {
+                        name: "",
+                        type: "In-Shop",
+                        shift: "11-3"
+                    },
+                    {
+                        name: "",
+                        type: "In-Shop",
+                        shift: "11-3"
+                    },
+                    {
+                        name: "Zach",
+                        type: "Manager",
+                        shift: "7-3"
+                    },
+                    {
+                        name: "Micheal",
+                        type: "Manager",
+                        shift: "12-8"
+                    }
+                ]
+            },
+            {
+                day: "Wednesday",
+                employees: [
+                    {
+                        name: "Bryce",
+                        type: "Driver",
+                        shift: "11:30-8"
+                    },
+                    {
+                        name: "Reid",
+                        type: "Driver",
+                        shift: "11-2"
+                    },
+                    {
+                        name: "Aaron",
+                        type: "Driver",
+                        shift: "10-3"
+                    },
+                    {
+                        name: "",
+                        type: "Driver",
+                        shift: "11-3"
+                    },
+                    {
+                        name: "",
+                        type: "Driver",
+                        shift: "11-3"
+                    },
+                    {
+                        name: "Jed",
+                        type: "In-Shop",
+                        shift: "11-3"
+                    },
+                    {
+                        name: "",
+                        type: "In-Shop",
+                        shift: "11-3"
+                    },
+                    {
+                        name: "",
+                        type: "In-Shop",
+                        shift: "11-3"
+                    },
+                    {
+                        name: "Zach",
+                        type: "Manager",
+                        shift: "7-3"
+                    },
+                    {
+                        name: "Micheal",
+                        type: "Manager",
+                        shift: "12-8"
+                    }
+                ]
+            },
+            {
+                day: "Thursday",
+                employees: [
+                    {
+                        name: "Bryce",
+                        type: "Driver",
+                        shift: "11-4"
+                    },
+                    {
+                        name: "John",
+                        type: "Driver",
+                        shift: "10-2"
+                    },
+                    {
+                        name: "Aaron",
+                        type: "Driver",
+                        shift: "12-8"
+                    },
+                    {
+                        name: "",
+                        type: "Driver",
+                        shift: "11-3"
+                    },
+                    {
+                        name: "",
+                        type: "Driver",
+                        shift: "11-3"
+                    },
+                    {
+                        name: "Jed",
+                        type: "In-Shop",
+                        shift: "11-2"
+                    },
+                    {
+                        name: "",
+                        type: "In-Shop",
+                        shift: "11-3"
+                    },
+                    {
+                        name: "",
+                        type: "In-Shop",
+                        shift: "11-3"
+                    },
+                    {
+                        name: "Zach",
+                        type: "Manager",
+                        shift: "7-3"
+                    },
+                    {
+                        name: "Micheal",
+                        type: "Manager",
+                        shift: "12-8"
+                    }
+                ]
+            },
+            {
+                day: "Friday",
+                employees: [
+                    {
+                        name: "Aaron",
+                        type: "Driver",
+                        shift: "11-2"
+                    },
+                    {
+                        name: "Reid",
+                        type: "Driver",
+                        shift: "11-3"
+                    },
+                    {
+                        name: "Bryce",
+                        type: "Driver",
+                        shift: "11-4"
+                    },
+                    {
+                        name: "",
+                        type: "Driver",
+                        shift: "11-3"
+                    },
+                    {
+                        name: "",
+                        type: "Driver",
+                        shift: "11-3"
+                    },
+                    {
+                        name: "James",
+                        type: "In-Shop",
+                        shift: "11-4"
+                    },
+                    {
+                        name: "",
+                        type: "In-Shop",
+                        shift: "11-3"
+                    },
+                    {
+                        name: "",
+                        type: "In-Shop",
+                        shift: "11-3"
+                    },
+                    {
+                        name: "Zach",
+                        type: "Manager",
+                        shift: "7-3"
+                    },
+                    {
+                        name: "Micheal",
+                        type: "Manager",
+                        shift: "12-8"
+                    }
+                ]
+            },
+            {
+                day: "Saturday",
+                employees: [
+                    {
+                        name: "Aaron",
+                        type: "Driver",
+                        shift: "10-5"
+                    },
+                    {
+                        name: "Reid",
+                        type: "Driver",
+                        shift: "12-7"
+                    },
+                    {
+                        name: "",
+                        type: "Driver",
+                        shift: "10-3"
+                    },
+                    {
+                        name: "",
+                        type: "Driver",
+                        shift: "11-3"
+                    },
+                    {
+                        name: "",
+                        type: "Driver",
+                        shift: "11-3"
+                    },
+                    {
+                        name: "",
+                        type: "In-Shop",
+                        shift: "11-3"
+                    },
+                    {
+                        name: "",
+                        type: "In-Shop",
+                        shift: "11-3"
+                    },
+                    {
+                        name: "",
+                        type: "In-Shop",
+                        shift: "11-3"
+                    },
+                    {
+                        name: "James",
+                        type: "Manager",
+                        shift: "7-3"
+                    },
+                    {
+                        name: "Jed",
+                        type: "Manager",
+                        shift: "12-8"
+                    }
+                ]
+            },
+            {
+                day: "Sunday",
+                employees: [
+                    {
+                        name: "Bryce",
+                        type: "Driver",
+                        shift: "10-7"
+                    },
+                    {
+                        name: "Reid",
+                        type: "Driver",
+                        shift: "11-4"
+                    },
+                    {
+                        name: "",
+                        type: "Driver",
+                        shift: "10-3"
+                    },
+                    {
+                        name: "",
+                        type: "Driver",
+                        shift: "11-3"
+                    },
+                    {
+                        name: "",
+                        type: "Driver",
+                        shift: "11-3"
+                    },
+                    {
+                        name: "",
+                        type: "In-Shop",
+                        shift: "11-3"
+                    },
+                    {
+                        name: "",
+                        type: "In-Shop",
+                        shift: "11-3"
+                    },
+                    {
+                        name: "",
+                        type: "In-Shop",
+                        shift: "11-3"
+                    },
+                    {
+                        name: "James",
+                        type: "Manager",
+                        shift: "7-3"
+                    },
+                    {
+                        name: "Jed",
+                        type: "Manager",
+                        shift: "12-8"
+                    }
+                ]
+            }
+        ]
+    }
